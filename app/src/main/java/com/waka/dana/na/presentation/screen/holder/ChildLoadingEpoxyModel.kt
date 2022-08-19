9d343f30aa431346cbe12217fdb4513c4a093efa
hvngoc <hvngoc94@gmail.com>
2022-08-19 14:44:54 +0700

    shimmer

--- app/src/main/java/com/waka/dana/na/presentation/screen/holder/ChildLoadingEpoxyModel.kt
+++ app/src/main/java/com/waka/dana/na/presentation/screen/holder/ChildLoadingEpoxyModel.kt
@@ -1,7 +1,6 @@
 package com.waka.dana.na.presentation.screen.holder
 
 import android.view.View
-import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
@@ -12,27 +11,11 @@ import com.waka.dana.na.databinding.ItemChildBinding
  * Created by hvngoc on 7/29/22
  */
 
-@EpoxyModelClass(layout = R.layout.item_child)
-abstract class ChildEpoxyModel :
-    EpoxyModelWithHolder<ChildEpoxyModel.ChildHolder>() {
-
-    @EpoxyAttribute
-    var date: String? = null
-
-    @EpoxyAttribute
-    var temperature: String? = null
-
-    @EpoxyAttribute
-    var pressure: String? = null
-
-    @EpoxyAttribute
-    var humidity: String? = null
-
-    @EpoxyAttribute
-    var description: String? = null
-
+@EpoxyModelClass(layout = R.layout.item_child_loading)
+abstract class ChildLoadingEpoxyModel :
+    EpoxyModelWithHolder<ChildLoadingEpoxyModel.ChildHolder>() {
     override fun bind(holder: ChildHolder) {
-        val context = holder.binding.root.context
+        //DOnt care
     }
 
     class ChildHolder : EpoxyHolder() {
